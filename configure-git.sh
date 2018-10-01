5c5ec1c4a11846a40be65b3a82d9f8899a4df5d2
Aleksander Bartnikiewicz <aleksander@bartnikiewi.cz>
2018-10-01 14:39:51 +0200

    the git configuration

---
+++ configure-git.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.curbra 'symbolic-ref --short HEAD'
+git config --global user.name='Aleksander Bartnikiewicz'
