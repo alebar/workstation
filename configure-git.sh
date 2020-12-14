6fd1b4cfc18c3fdf4fdfe260ae826b757842f414
Aleksander Bartnikiewicz <aleksander.bartnikiewicz@autenti.com>
2020-12-14 22:10:30 +0100

    one more git global option

--- configure-git.sh
+++ configure-git.sh
@@ -4,3 +4,4 @@ git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.curbra 'symbolic-ref --short HEAD'
 git config --global user.name 'Aleksander Bartnikiewicz'
+git config --global core.editor 'vim'
